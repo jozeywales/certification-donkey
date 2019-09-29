00f419810c7c9870f220c3c0cf7099f67ea3830d
John Steele <jsteele@doterra.com>
2019-09-29 14:42:38 -0600

    Added Categories items using a while loop for the Challenge 3 assignment

--- challenges/challenge3/challenge3.py
+++ challenges/challenge3/challenge3.py
@@ -18,22 +18,37 @@ class Challenge3(unittest.TestCase):
         self.driver.get("https://www.copart.com")
         self.assertIn("Copart", self.driver.title)
 
-        # 1. Find the popular search links
+        # 1. Find the popular search captions and links
         try:
-            childSearchElements = WebDriverWait(self.driver, 30).until(
+            childSearchElementsMakesModels = WebDriverWait(self.driver, 30).until(
                 EC.presence_of_all_elements_located((By.XPATH, "//a[starts-with(@href,'./popular/m')]"))
         )
         except TimeoutError:
             print("an exception occurred" + e)
 
-        for element in childSearchElements:
+        print("Makes/Models")
+        for element in childSearchElementsMakesModels:
             link = element.get_attribute("href")
             text = element.get_property("text")
             print(text + ' ' + link)
 
+        self.assertCountEqual(str(len(childSearchElementsMakesModels)),"20","The count of Makes/Models is not 20")
 
-        self.assertCountEqual(str(len(childSearchElements)),"20","The count of popular models is not 20")
+        # 2. Find the Categories captions and links
+        try:
+            childSearchElementsCategories = WebDriverWait(self.driver, 30).until(
+                EC.presence_of_all_elements_located((By.XPATH, "//*[@id='tabTrending']/div[3]//a"))
+        )
+        except TimeoutError:
+            print("an exception occurred" + e)
 
+        i = 0
+        print("\nCategories")
+        while i < len(childSearchElementsCategories):
+            print(childSearchElementsCategories[i].text + " " + str(childSearchElementsCategories[i].get_attribute("href")))
+            i += 1
+
+        self.assertCountEqual(str(len(childSearchElementsCategories)),"20","The count of Categories is not 20")
 
 
 if __name__ == '__main__':
