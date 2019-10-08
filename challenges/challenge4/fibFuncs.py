b3a33119e73d4531f4a4246001c48de6b01456a8
John Steele <jsteele@doterra.com>
2019-10-07 21:06:21 -0600

    Challenge4: experimenting with changes to fibonacci method

--- challenges/challenge4/fibFuncs.py
+++ challenges/challenge4/fibFuncs.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, ceil
 
 class FibMethods():
     '''A simple class that contains a fibonacci method'''
@@ -6,9 +6,18 @@ class FibMethods():
     def __init__(self):
         '''initialize'''
 
-
-
     def fibFormula(self, fibNum):
         if fibNum == 0:
             return 1
         return ((1+sqrt(5))**fibNum-(1-sqrt(5))**fibNum)/(2**fibNum*sqrt(5))
+
+    def myFib(self, fibNum):
+        if fibNum <= 1:
+            return fibNum
+        else:
+            return(self.myFib(fibNum-1) + self.myFib(fibNum-2))
+
+
+myTest = FibMethods()
+
+myTest.myFib(5)
