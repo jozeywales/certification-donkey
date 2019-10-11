47ecda30d556daedaf841fd6f35ae9387966c1f6
John Steele <jsteele@doterra.com>
2019-10-10 23:16:57 -0600

    Challenge5: List of models is working, conversion to a set in progress.

--- challenges/challenge5/challenge5.py
+++ challenges/challenge5/challenge5.py
@@ -1,6 +1,7 @@
 import unittest
 from cmath import e
 from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,25 +22,41 @@ class Challenge5(unittest.TestCase):
         self.assertIn("Copart", self.driver.title)
 
         searchElement = self.driver.find_element(By.ID, "input-search")
-        searchElement.send_keys("PORSCHE" + Keys.ENTER)
+        searchElement.send_keys("PORSCHE")
+        searchButton = self.driver.find_element(By.XPATH, "//button[@data-uname='homepageHeadersearchsubmit']")
+        searchButton.click()
 
-        ddElement = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_all_elements_located((By.XPATH, "//div[@id='serverSideDataTable_length']/label/select")))
+        ddElement = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//div[@id='serverSideDataTable_length']//select[@name='serverSideDataTable_length']")))
 
         select_list = WebDriverSelect(ddElement)
-        select_list.select_by_index()
-        selected_option = select_list.deselect_by_index(2)
+        select_list.select_by_value('100')
+        selected_option = select_list.first_selected_option.text
+        assert selected_option == '100', "The Show dropdown select option is not 100 entries"
 
         porscheModelList = []
 
         # 1. Find all the models of Porsche
-        searchPorscheModels = self.driver.find_elements_by_xpath(
-            "//span[@data-uname='lotsearchLotmodel']/../span/text()")
+        searchPorscheModels = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//span[@data-uname='lotsearchLotmodel']/../span")))
 
-        for element in searchPorscheModels:
-            porscheModelList.append(element)
+        try:
+            for element in searchPorscheModels:
+                porscheModelList.append(element.text)
+        except:
+            print(StaleElementReferenceException)
 
-        print(porscheModelList)
+        porscheModelSet = set(porscheModelList)
+
+        print("The number of distinct porsche models is: " + str(len(porscheModelSet)))
+        print()
+
+
+
+
+        #for model in porscheModelList:
+        #    print(model)
 
 
 if __name__ == '__main__':
