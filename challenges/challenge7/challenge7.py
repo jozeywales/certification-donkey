da17610f3b25925c07b2f42b4367e20e41fb7e48
John Steele <jsteele@doterra.com>
2019-12-06 17:36:24 -0700

    Challenge7-DriverManager, just added a few comments for browser drivers that won't work.

--- challenges/challenge7/challenge7.py
+++ challenges/challenge7/challenge7.py
@@ -11,11 +11,11 @@ class Challenge7(unittest.TestCase, DriverManager):
         # self.driver = eval(driverManager.getDriver("chrome", "http://www.copart.com"))
         self.driver = eval(driverManager.getDriver("chrome"))
         # self.driver = eval(driverManager.getDriver("firefox"))
-        # self.driver = eval(driverManager.getDriver("ie"))
+        # self.driver = eval(driverManager.getDriver("ie"))  # doesn't work even after making recommended IE config changes
         # self.driver = eval(driverManager.getDriver("opera"))
         # self.driver = eval(driverManager.getDriver("edge"))
-        # self.driver = eval(driverManager.getDriver("safari"))
-        # self.driver = eval(driverManager.getDriver("phantomjs"))
+        # self.driver = eval(driverManager.getDriver("safari")) # doesn't work, my laptop is Windows 10
+        # self.driver = eval(driverManager.getDriver("phantomjs")) # works but message stating to use 'headless' instead
 
 
     def tearDown(self):
